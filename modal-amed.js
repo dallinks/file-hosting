c78015350540f6bd37ea8a4cea573d86e943a752
dallinks <90439712+dallinks@users.noreply.github.com>
2023-03-20 19:16:58 -0600

    Update modal-amed.js

--- modal-amed.js
+++ modal-amed.js
@@ -1,4 +1,4 @@
-if(document.querySelector('#interestButton'){  
+if(!document.querySelector('#interestButton'){  
   const submitButton = document.getElementById('SubmitButton');
   submitButton.addEventListener('click', function submitButton(){
     if((document.getElementById('name').value) == '' || (document.getElementById('email').value) == '' || (document.getElementById('phone').value) == ''){
